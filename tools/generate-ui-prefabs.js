d507dcf5b9014f35ddfd4c83c6f6c6fd32cbe069
xiejiayang <xiejiayang@users.noreply.github.com>
2026-06-26 23:30:14 +0800

    feat(5.3): generate Boot/Title/LevelMap/Game scene placeholders

--- tools/generate-ui-prefabs.js
+++ tools/generate-ui-prefabs.js
@@ -1,5 +1,5 @@
 /**
- * UI 预制体 JSON 生成器
+ * UI 预制体与场景 JSON 生成器
  * 基于 Cocos Creator 3.8.x 序列化格式
  */
 const fs = require('fs');
@@ -9,6 +9,14 @@ const crypto = require('crypto');
 const CLIENT_DIR = path.join(__dirname, '..', 'client');
 const UI_PREFAB_DIR = path.join(CLIENT_DIR, 'assets', 'prefabs', 'ui');
 const GAME_PREFAB_DIR = path.join(CLIENT_DIR, 'assets', 'prefabs', 'game');
+const SCENE_DIR = path.join(CLIENT_DIR, 'assets', 'scenes');
+
+const SCRIPT_UUIDS = {
+  BootSceneCtrl: 'a47c985b-f77c-4a00-a9c5-054d744af5d6',
+  TitleSceneCtrl: '3123c334-8968-46d3-8227-967978fd646f',
+  LevelMapSceneCtrl: '5b853fbf-6939-4cc5-a374-11fd2b065bd5',
+  GameSceneCtrl: 'eb6d4b3f-6e49-48da-95a7-287ff633ddfb'
+};
 
 function uuid() {
   return crypto.randomUUID();
@@ -43,22 +51,9 @@ function size(w, h) {
   return { __type__: 'cc.Size', width: w, height: h };
 }
 
-class PrefabBuilder {
-  constructor(name) {
-    this.name = name;
+class BaseBuilder {
+  constructor() {
     this.objects = [];
-    this.rootId = null;
-    // Placeholder prefab at index 0; data will point to root
-    this.prefabId = this.add({
-      __type__: 'cc.Prefab',
-      _name: name,
-      _objFlags: 0,
-      __editorExtras__: {},
-      _native: '',
-      data: { __id__: 0 },
-      optimizationPolicy: 0,
-      persistent: false
-    });
   }
 
   add(obj) {
@@ -72,7 +67,7 @@ class PrefabBuilder {
       _name: name,
       _objFlags: 0,
       __editorExtras__: {},
-      _parent: parent ? { __id__: parent } : null,
+      _parent: parent !== null ? { __id__: parent } : null,
       _children: children.map(id => ({ __id__: id })),
       _active: true,
       _components: [],
@@ -87,11 +82,6 @@ class PrefabBuilder {
     });
   }
 
-  addRoot({ name, pos = vec3(0, 0), rot = quat(), eulerAngles = euler(), scale = vec3(1, 1, 1) }) {
-    this.rootId = this.addNode({ name, pos, rot, eulerAngles, scale });
-    return this.rootId;
-  }
-
   addChild(parentId, { name, pos = vec3(0, 0), rot = quat(), eulerAngles = euler(), scale = vec3(1, 1, 1) }) {
     const childId = this.addNode({ name, parent: parentId, pos, rot, eulerAngles, scale });
     this.objects[parentId]._children.push({ __id__: childId });
@@ -101,7 +91,6 @@ class PrefabBuilder {
   setComponents(nodeId, nodeSize, componentIds) {
     const uiTransformId = this.addUITransform(nodeId, nodeSize);
     this.objects[nodeId]._components = [uiTransformId, ...componentIds].map(id => ({ __id__: id }));
-    this.objects[nodeId]._prefab = { __id__: this.addPrefabInfo(nodeId) };
   }
 
   addUITransform(nodeId, contentSize, anchor = vec2(0.5, 0.5)) {
@@ -213,6 +202,19 @@ class PrefabBuilder {
     });
   }
 
+  addScript(nodeId, className) {
+    return this.add({
+      __type__: className,
+      _name: '',
+      _objFlags: 0,
+      __editorExtras__: {},
+      node: { __id__: nodeId },
+      _enabled: true,
+      __prefab: null,
+      _id: ''
+    });
+  }
+
   addCompPrefabInfo() {
     return this.add({
       __type__: 'cc.CompPrefabInfo',
@@ -231,9 +233,135 @@ class PrefabBuilder {
       nestedPrefabInstanceRoots: null
     });
   }
+}
+
+class PrefabBuilder extends BaseBuilder {
+  constructor(name) {
+    super();
+    this.name = name;
+    this.rootId = null;
+    this.prefabId = this.add({
+      __type__: 'cc.Prefab',
+      _name: name,
+      _objFlags: 0,
+      __editorExtras__: {},
+      _native: '',
+      data: { __id__: 0 },
+      optimizationPolicy: 0,
+      persistent: false
+    });
+  }
+
+  addRoot({ name, pos = vec3(0, 0), rot = quat(), eulerAngles = euler(), scale = vec3(1, 1, 1) }) {
+    this.rootId = this.addNode({ name, pos, rot, eulerAngles, scale });
+    return this.rootId;
+  }
+
+  setComponents(nodeId, nodeSize, componentIds) {
+    super.setComponents(nodeId, nodeSize, componentIds);
+    this.objects[nodeId]._prefab = { __id__: this.addPrefabInfo(nodeId) };
+  }
 
   finalize() {
     this.objects[this.prefabId].data.__id__ = this.rootId;
+    return this.objects;
+  }
+}
+
+class SceneBuilder extends BaseBuilder {
+  constructor(name) {
+    super();
+    this.name = name;
+    this.canvasId = null;
+    this.cameraId = null;
+    this.sceneAssetId = this.add({
+      __type__: 'cc.SceneAsset',
+      _name: name,
+      _objFlags: 0,
+      __editorExtras__: {},
+      _native: '',
+      scene: { __id__: 0 },
+      _id: ''
+    });
+  }
+
+  buildCanvas() {
+    const sceneId = this.add({
+      __type__: 'cc.Scene',
+      _name: this.name,
+      _objFlags: 0,
+      _parent: null,
+      _children: [],
+      _active: true,
+      _components: [],
+      _prefab: null,
+      autoReleaseAssets: false,
+      _id: ''
+    });
+
+    const canvasId = this.addNode({ name: 'Canvas', parent: sceneId });
+    this.canvasId = canvasId;
+
+    const cameraId = this.addChild(canvasId, { name: 'Camera', pos: vec3(0, 0, 1000) });
+    this.cameraId = cameraId;
+
+    const cameraCompId = this.add({
+      __type__: 'cc.Camera',
+      _name: '',
+      _objFlags: 0,
+      __editorExtras__: {},
+      node: { __id__: cameraId },
+      _enabled: true,
+      __prefab: null,
+      _projection: 0,
+      _priority: 0,
+      _fov: 45,
+      _fovAxis: 0,
+      _orthoHeight: 10,
+      _near: 0,
+      _far: 1000,
+      _color: color(0, 0, 0, 255),
+      _depth: 1,
+      _stencil: 0,
+      _clearFlags: 6,
+      _rect: { __type__: 'cc.Rect', x: 0, y: 0, width: 1, height: 1 },
+      _aperture: 19,
+      _shutter: 7,
+      _iso: 0,
+      _screenScale: 1,
+      _visibility: 1108344832,
+      _targetTexture: null,
+      _id: ''
+    });
+
+    const canvasTransformId = this.addUITransform(canvasId, size(1280, 720));
+    const canvasCompId = this.add({
+      __type__: 'cc.Canvas',
+      _name: '',
+      _objFlags: 0,
+      __editorExtras__: {},
+      node: { __id__: canvasId },
+      _enabled: true,
+      __prefab: null,
+      _cameraComponent: { __id__: cameraCompId },
+      _alignCanvasWithScreen: true,
+      _id: ''
+    });
+
+    this.objects[sceneId]._children.push({ __id__: canvasId });
+    this.objects[cameraId]._components.push({ __id__: cameraCompId });
+    this.objects[canvasId]._components.push({ __id__: canvasTransformId }, { __id__: canvasCompId });
+
+    return canvasId;
+  }
+
+  addUICanvasScript(className) {
+    const scriptId = this.addScript(this.canvasId, className);
+    this.objects[this.canvasId]._components.push({ __id__: scriptId });
+  }
+
+  finalize() {
+    this.objects[this.sceneAssetId].scene.__id__ = 1;
     return this.objects;
   }
 }
@@ -265,6 +393,23 @@ function savePrefab(dir, name, objects) {
     files: ['.json'],
     subMetas: {},
     userData: { syncNodeName: name }
+  };
+  fs.writeFileSync(metaPath, JSON.stringify(meta, null, 2));
+  console.log(`Generated: ${filePath}`);
+}
+
+function saveScene(dir, name, objects) {
+  const filePath = path.join(dir, `${name}.scene`);
+  const metaPath = `${filePath}.meta`;
+  fs.writeFileSync(filePath, JSON.stringify(objects, null, 2));
+  const meta = {
+    ver: '1.1.50',
+    importer: 'scene',
+    imported: true,
+    uuid: uuid(),
+    files: [`${name}.scene.json`],
+    subMetas: {},
+    userData: {}
   };
   fs.writeFileSync(metaPath, JSON.stringify(meta, null, 2));
   console.log(`Generated: ${filePath}`);
@@ -403,8 +548,110 @@ function buildBambooCage() {
   return b.finalize();
 }
 
+function buildBootScene() {
+  const b = new SceneBuilder('BootScene');
+  b.buildCanvas();
+  b.addUICanvasScript('BootSceneCtrl');
+
+  const titleId = b.addChild(b.canvasId, { name: 'Title', pos: vec3(0, 120) });
+  b.setComponents(titleId, size(600, 80), [b.addLabel(titleId, '都江堰治水', 56, PALETTE.inkDark, 72)]);
+
+  const subtitleId = b.addChild(b.canvasId, { name: 'Subtitle', pos: vec3(0, 40) });
+  b.setComponents(subtitleId, size(600, 40), [b.addLabel(subtitleId, '国风水利解谜', 24, PALETTE.inkGray, 32)]);
+
+  const progressId = b.addChild(b.canvasId, { name: 'ProgressBar', pos: vec3(0, -80) });
+  b.setComponents(progressId, size(400, 12), [b.addSprite(progressId, PALETTE.inkDark)]);
+
+  const loadingId = b.addChild(b.canvasId, { name: 'LoadingText', pos: vec3(0, -120) });
+  b.setComponents(loadingId, size(400, 32), [b.addLabel(loadingId, '正在引水入渠…', 20, PALETTE.inkGray, 32)]);
+
+  return b.finalize();
+}
+
+function buildTitleScene() {
+  const b = new SceneBuilder('TitleScene');
+  b.buildCanvas();
+  b.addUICanvasScript('TitleSceneCtrl');
+
+  const titleId = b.addChild(b.canvasId, { name: 'Title', pos: vec3(0, 80) });
+  b.setComponents(titleId, size(600, 80), [b.addLabel(titleId, '都江堰治水', 64, PALETTE.inkDark, 80)]);
+
+  const startId = b.addChild(b.canvasId, { name: 'StartButton', pos: vec3(0, -40) });
+  b.setComponents(startId, size(200, 56), [b.addSprite(startId, PALETTE.paper), b.addButton(startId, PALETTE.paper, PALETTE.paperDark, PALETTE.white, PALETTE.inkLight)]);
+  const startLabelId = b.addChild(startId, { name: 'Label' });
+  b.setComponents(startLabelId, size(200, 56), [b.addLabel(startLabelId, '开始治水', 28, PALETTE.inkDark, 40)]);
+
+  const settingsId = b.addChild(b.canvasId, { name: 'SettingsButton', pos: vec3(260, 140) });
+  b.setComponents(settingsId, size(64, 64), [b.addSprite(settingsId, PALETTE.paperDark), b.addButton(settingsId, PALETTE.paperDark, PALETTE.paper, PALETTE.white, PALETTE.inkLight)]);
+  const settingsLabelId = b.addChild(settingsId, { name: 'Label' });
+  b.setComponents(settingsLabelId, size(64, 64), [b.addLabel(settingsLabelId, '设置', 20, PALETTE.inkDark, 40)]);
+
+  return b.finalize();
+}
+
+function buildLevelMapScene() {
+  const b = new SceneBuilder('LevelMapScene');
+  b.buildCanvas();
+  b.addUICanvasScript('LevelMapSceneCtrl');
+
+  const titleId = b.addChild(b.canvasId, { name: 'Title', pos: vec3(-440, 280) });
+  b.setComponents(titleId, size(240, 48), [b.addLabel(titleId, '治水卷轴', 36, PALETTE.inkDark, 48)]);
+
+  const backId = b.addChild(b.canvasId, { name: 'BackButton', pos: vec3(-560, 280) });
+  b.setComponents(backId, size(80, 40), [b.addSprite(backId, PALETTE.paper), b.addButton(backId, PALETTE.paper, PALETTE.paperDark, PALETTE.white, PALETTE.inkLight)]);
+  const backLabelId = b.addChild(backId, { name: 'Label' });
+  b.setComponents(backLabelId, size(80, 40), [b.addLabel(backLabelId, '返回', 18, PALETTE.inkDark, 28)]);
+
+  const node1Id = b.addChild(b.canvasId, { name: 'LevelNode1', pos: vec3(-200, 0) });
+  b.setComponents(node1Id, size(80, 80), [b.addSprite(node1Id, PALETTE.paper)]);
+  const node1LabelId = b.addChild(node1Id, { name: 'Label' });
+  b.setComponents(node1LabelId, size(80, 80), [b.addLabel(node1LabelId, '1', 28, PALETTE.inkDark, 80)]);
+
+  const node2Id = b.addChild(b.canvasId, { name: 'LevelNode2', pos: vec3(0, 0) });
+  b.setComponents(node2Id, size(80, 80), [b.addSprite(node2Id, PALETTE.ochre)]);
+  const node2LabelId = b.addChild(node2Id, { name: 'Label' });
+  b.setComponents(node2LabelId, size(80, 80), [b.addLabel(node2LabelId, '2', 28, PALETTE.inkDark, 80)]);
+
+  return b.finalize();
+}
+
+function buildGameScene() {
+  const b = new SceneBuilder('GameScene');
+  b.buildCanvas();
+  b.addUICanvasScript('GameSceneCtrl');
+
+  const headerId = b.addChild(b.canvasId, { name: 'Header', pos: vec3(0, 310) });
+  b.setComponents(headerId, size(1280, 80), [b.addSprite(headerId, PALETTE.paperDark)]);
+
+  const backId = b.addChild(headerId, { name: 'BackButton', pos: vec3(-560, 0) });
+  b.setComponents(backId, size(80, 40), [b.addSprite(backId, PALETTE.paper), b.addButton(backId, PALETTE.paper, PALETTE.paperDark, PALETTE.white, PALETTE.inkLight)]);
+  const backLabelId = b.addChild(backId, { name: 'Label' });
+  b.setComponents(backLabelId, size(80, 40), [b.addLabel(backLabelId, '返回', 18, PALETTE.inkDark, 28)]);
+
+  const levelNameId = b.addChild(headerId, { name: 'LevelName', pos: vec3(-380, 0) });
+  b.setComponents(levelNameId, size(200, 40), [b.addLabel(levelNameId, 'L1 堵', 28, PALETTE.inkDark, 40)]);
+
+  const moneyId = b.addChild(headerId, { name: 'MoneyBar', pos: vec3(420, 0) });
+  b.setComponents(moneyId, size(140, 36), [b.addSprite(moneyId, PALETTE.paper)]);
+  const moneyLabelId = b.addChild(moneyId, { name: 'Label' });
+  b.setComponents(moneyLabelId, size(120, 32), [b.addLabel(moneyLabelId, '100', 24, PALETTE.inkDark, 32)]);
+
+  const boardId = b.addChild(b.canvasId, { name: 'GameBoard', pos: vec3(0, 20) });
+  b.setComponents(boardId, size(1200, 480), [b.addSprite(boardId, PALETTE.paper)]);
+
+  const toolbarId = b.addChild(b.canvasId, { name: 'Toolbar', pos: vec3(0, -280) });
+  b.setComponents(toolbarId, size(1280, 120), [b.addSprite(toolbarId, PALETTE.paperDark)]);
+
+  const waterBtnId = b.addChild(toolbarId, { name: 'WaterButton', pos: vec3(560, 0) });
+  b.setComponents(waterBtnId, size(64, 64), [b.addSprite(waterBtnId, PALETTE.water), b.addButton(waterBtnId, PALETTE.water, PALETTE.waterLight, PALETTE.waterLight, PALETTE.inkLight)]);
+  const waterLabelId = b.addChild(waterBtnId, { name: 'Label' });
+  b.setComponents(waterLabelId, size(64, 64), [b.addLabel(waterLabelId, '放水', 18, PALETTE.paper, 64)]);
+
+  return b.finalize();
+}
+
 function main() {
-  [UI_PREFAB_DIR, GAME_PREFAB_DIR].forEach(dir => {
+  [UI_PREFAB_DIR, GAME_PREFAB_DIR, SCENE_DIR].forEach(dir => {
     if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
   });
 
@@ -420,7 +667,12 @@ function main() {
   savePrefab(GAME_PREFAB_DIR, 'StoneWall', buildStoneWall());
   savePrefab(GAME_PREFAB_DIR, 'BambooCage', buildBambooCage());
 
-  console.log('All prefabs generated.');
+  saveScene(SCENE_DIR, 'BootScene', buildBootScene());
+  saveScene(SCENE_DIR, 'TitleScene', buildTitleScene());
+  saveScene(SCENE_DIR, 'LevelMapScene', buildLevelMapScene());
+  saveScene(SCENE_DIR, 'GameScene', buildGameScene());
+
+  console.log('All prefabs and scenes generated.');
 }
 
 main();
