28e53329fdd5387a453dbbe7d2b8d270377b6c7d
xiejiayang <xiejiayang@users.noreply.github.com>
2026-06-26 23:11:42 +0800

    fix(prefabs): correct prefab JSON serialization order and Button component fields

--- tools/generate-ui-prefabs.js
+++ tools/generate-ui-prefabs.js
@@ -47,6 +47,18 @@ class PrefabBuilder {
   constructor(name) {
     this.name = name;
     this.objects = [];
+    this.rootId = null;
+    // Placeholder prefab at index 0; data will point to root
+    this.prefabId = this.add({
+      __type__: 'cc.Prefab',
+      _name: name,
+      _objFlags: 0,
+      __editorExtras__: {},
+      _native: '',
+      data: { __id__: 0 },
+      optimizationPolicy: 0,
+      persistent: false
+    });
   }
 
   add(obj) {
@@ -75,14 +87,15 @@ class PrefabBuilder {
     });
   }
 
-  setParent(childId, parentId) {
-    this.objects[childId]._parent = { __id__: parentId };
+  addRoot({ name, pos = vec3(0, 0), rot = quat(), eulerAngles = euler(), scale = vec3(1, 1, 1) }) {
+    this.rootId = this.addNode({ name, pos, rot, eulerAngles, scale });
+    return this.rootId;
   }
 
-  buildRoot({ name, children = [], pos = vec3(0, 0), rot = quat(), eulerAngles = euler(), scale = vec3(1, 1, 1) }) {
-    const rootId = this.addNode({ name, children, pos, rot, eulerAngles, scale });
-    children.forEach(childId => this.setParent(childId, rootId));
-    return rootId;
+  addChild(parentId, { name, pos = vec3(0, 0), rot = quat(), eulerAngles = euler(), scale = vec3(1, 1, 1) }) {
+    const childId = this.addNode({ name, parent: parentId, pos, rot, eulerAngles, scale });
+    this.objects[parentId]._children.push({ __id__: childId });
+    return childId;
   }
 
   setComponents(nodeId, nodeSize, componentIds) {
@@ -183,22 +196,8 @@ class PrefabBuilder {
       node: { __id__: nodeId },
       _enabled: true,
       __prefab: { __id__: this.addCompPrefabInfo() },
-      _customMaterial: null,
-      _srcBlendFactor: 2,
-      _dstBlendFactor: 4,
-      _color: color(255, 255, 255),
-      _spriteFrame: null,
-      _type: 1,
-      _fillType: 0,
-      _sizeMode: 0,
-      _fillCenter: vec2(0, 0),
-      _fillStart: 0,
-      _fillRange: 0,
-      _isTrimmedMode: true,
-      _useGrayscale: false,
-      _atlas: null,
       _id: '',
-      _transition: 2,
+      _transition: 1,
       _normalColor: normalColor,
       _hoverColor: hoverColor,
       _pressedColor: pressedColor,
@@ -209,6 +208,7 @@ class PrefabBuilder {
       _disabledSprite: null,
       _duration: 0.1,
       _zoomScale: 1.2,
+      _target: { __id__: nodeId },
       _clickEvents: []
     });
   }
@@ -232,20 +232,8 @@ class PrefabBuilder {
     });
   }
 
-  finalize(rootNodeId) {
-    const prefabId = this.add({
-      __type__: 'cc.Prefab',
-      _name: this.name,
-      _objFlags: 0,
-      __editorExtras__: {},
-      _native: '',
-      data: { __id__: rootNodeId },
-      optimizationPolicy: 0,
-      persistent: false
-    });
-    // Move prefab to front
-    const prefabObj = this.objects.pop();
-    this.objects.unshift(prefabObj);
+  finalize() {
+    this.objects[this.prefabId].data.__id__ = this.rootId;
     return this.objects;
   }
 }
@@ -284,130 +272,129 @@ function savePrefab(dir, name, objects) {
 
 function buildButton() {
   const b = new PrefabBuilder('Button');
-  const labelId = b.addNode({ name: 'Label' });
+  const rootId = b.addRoot({ name: 'Button' });
+  const labelId = b.addChild(rootId, { name: 'Label' });
   const labelCompId = b.addLabel(labelId, '按钮', 24, PALETTE.inkDark, 32);
   b.setComponents(labelId, size(160, 48), [labelCompId]);
-  const rootId = b.buildRoot({ name: 'Button', children: [labelId] });
   const spriteId = b.addSprite(rootId, PALETTE.paper);
   const buttonId = b.addButton(rootId, PALETTE.paper, PALETTE.paperDark, PALETTE.white, PALETTE.inkLight);
   b.setComponents(rootId, size(160, 48), [spriteId, buttonId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildToast() {
   const b = new PrefabBuilder('Toast');
-  const labelId = b.addNode({ name: 'Label' });
+  const rootId = b.addRoot({ name: 'Toast' });
+  const labelId = b.addChild(rootId, { name: 'Label' });
   const labelCompId = b.addLabel(labelId, '提示文本', 20, PALETTE.paper, 32);
   b.setComponents(labelId, size(280, 40), [labelCompId]);
-  const rootId = b.buildRoot({ name: 'Toast', children: [labelId] });
   const spriteId = b.addSprite(rootId, PALETTE.inkDark);
   b.setComponents(rootId, size(280, 40), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildScrollModal() {
   const b = new PrefabBuilder('ScrollModal');
-  const contentId = b.addNode({ name: 'Content', pos: vec3(0, 0) });
-  const contentLabelId = b.addLabel(contentId, '内容', 20, PALETTE.inkDark, 28);
-  b.setComponents(contentId, size(416, 240), [contentLabelId]);
-  const titleId = b.addNode({ name: 'Title', pos: vec3(0, 120) });
+  const rootId = b.addRoot({ name: 'ScrollModal' });
+  const titleId = b.addChild(rootId, { name: 'Title', pos: vec3(0, 120) });
   const titleLabelId = b.addLabel(titleId, '标题', 28, PALETTE.inkDark, 36);
   b.setComponents(titleId, size(416, 40), [titleLabelId]);
-  const rootId = b.buildRoot({ name: 'ScrollModal', children: [titleId, contentId] });
+  const contentId = b.addChild(rootId, { name: 'Content', pos: vec3(0, 0) });
+  const contentLabelId = b.addLabel(contentId, '内容', 20, PALETTE.inkDark, 28);
+  b.setComponents(contentId, size(416, 240), [contentLabelId]);
   const spriteId = b.addSprite(rootId, PALETTE.paper);
   b.setComponents(rootId, size(480, 320), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildSeal() {
   const b = new PrefabBuilder('Seal');
-  const labelId = b.addNode({ name: 'Label' });
-  const labelCompId = b.addLabel(labelId, '印', 20, PALETTE.paper, 48);
-  b.setComponents(labelId, size(48, 48), [labelCompId]);
-  const rootId = b.buildRoot({
+  const rootId = b.addRoot({
     name: 'Seal',
-    children: [labelId],
     rot: quat(0, 0, -0.052335956242943835, 0.9986295347545738),
     eulerAngles: euler(0, 0, -6)
   });
+  const labelId = b.addChild(rootId, { name: 'Label' });
+  const labelCompId = b.addLabel(labelId, '印', 20, PALETTE.paper, 48);
+  b.setComponents(labelId, size(48, 48), [labelCompId]);
   const spriteId = b.addSprite(rootId, PALETTE.redInk);
   b.setComponents(rootId, size(48, 48), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildLevelNode() {
   const b = new PrefabBuilder('LevelNode');
-  const labelId = b.addNode({ name: 'Label' });
+  const rootId = b.addRoot({ name: 'LevelNode' });
+  const labelId = b.addChild(rootId, { name: 'Label' });
   const labelCompId = b.addLabel(labelId, '1', 28, PALETTE.inkDark, 80);
   b.setComponents(labelId, size(80, 80), [labelCompId]);
-  const rootId = b.buildRoot({ name: 'LevelNode', children: [labelId] });
   const spriteId = b.addSprite(rootId, PALETTE.paper);
   b.setComponents(rootId, size(80, 80), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildMoneyBar() {
   const b = new PrefabBuilder('MoneyBar');
-  const labelId = b.addNode({ name: 'Label', pos: vec3(10, 0) });
+  const rootId = b.addRoot({ name: 'MoneyBar' });
+  const labelId = b.addChild(rootId, { name: 'Label', pos: vec3(10, 0) });
   const labelCompId = b.addLabel(labelId, '100', 24, PALETTE.inkDark, 32);
   b.setComponents(labelId, size(120, 32), [labelCompId]);
-  const rootId = b.buildRoot({ name: 'MoneyBar', children: [labelId] });
   const spriteId = b.addSprite(rootId, PALETTE.paperDark);
   b.setComponents(rootId, size(140, 36), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildBlockCard() {
   const b = new PrefabBuilder('BlockCard');
-  const countId = b.addNode({ name: 'Count', pos: vec3(0, -22) });
-  const countLabelId = b.addLabel(countId, 'x5', 18, PALETTE.inkDark, 20);
-  b.setComponents(countId, size(64, 20), [countLabelId]);
-  const nameId = b.addNode({ name: 'Name', pos: vec3(0, 22) });
+  const rootId = b.addRoot({ name: 'BlockCard' });
+  const nameId = b.addChild(rootId, { name: 'Name', pos: vec3(0, 22) });
   const nameLabelId = b.addLabel(nameId, '石墙', 16, PALETTE.inkDark, 20);
   b.setComponents(nameId, size(64, 20), [nameLabelId]);
-  const rootId = b.buildRoot({ name: 'BlockCard', children: [nameId, countId] });
+  const countId = b.addChild(rootId, { name: 'Count', pos: vec3(0, -22) });
+  const countLabelId = b.addLabel(countId, 'x5', 18, PALETTE.inkDark, 20);
+  b.setComponents(countId, size(64, 20), [countLabelId]);
   const spriteId = b.addSprite(rootId, PALETTE.paper);
   b.setComponents(rootId, size(64, 80), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildDeleteZone() {
   const b = new PrefabBuilder('DeleteZone');
-  const labelId = b.addNode({ name: 'Label' });
+  const rootId = b.addRoot({ name: 'DeleteZone' });
+  const labelId = b.addChild(rootId, { name: 'Label' });
   const labelCompId = b.addLabel(labelId, '拖\n删', 16, PALETTE.redInk, 20);
   b.setComponents(labelId, size(56, 56), [labelCompId]);
-  const rootId = b.buildRoot({ name: 'DeleteZone', children: [labelId] });
   const spriteId = b.addSprite(rootId, color(245, 240, 232, 128));
   b.setComponents(rootId, size(56, 56), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildWaterButton() {
   const b = new PrefabBuilder('WaterButton');
-  const labelId = b.addNode({ name: 'Label' });
+  const rootId = b.addRoot({ name: 'WaterButton' });
+  const labelId = b.addChild(rootId, { name: 'Label' });
   const labelCompId = b.addLabel(labelId, '放水', 20, PALETTE.paper, 64);
   b.setComponents(labelId, size(64, 64), [labelCompId]);
-  const rootId = b.buildRoot({ name: 'WaterButton', children: [labelId] });
   const spriteId = b.addSprite(rootId, PALETTE.water);
   const buttonId = b.addButton(rootId, PALETTE.water, PALETTE.waterLight, PALETTE.waterLight, PALETTE.inkLight);
   b.setComponents(rootId, size(64, 64), [spriteId, buttonId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildStoneWall() {
   const b = new PrefabBuilder('StoneWall');
-  const rootId = b.addNode({ name: 'StoneWall' });
+  const rootId = b.addRoot({ name: 'StoneWall' });
   const spriteId = b.addSprite(rootId, PALETTE.inkGray);
   b.setComponents(rootId, size(64, 64), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function buildBambooCage() {
   const b = new PrefabBuilder('BambooCage');
-  const rootId = b.addNode({ name: 'BambooCage' });
+  const rootId = b.addRoot({ name: 'BambooCage' });
   const spriteId = b.addSprite(rootId, PALETTE.greenPale);
   b.setComponents(rootId, size(64, 64), [spriteId]);
-  return b.finalize(rootId);
+  return b.finalize();
 }
 
 function main() {
